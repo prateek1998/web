7e847b56bb8920df8d34bda0b3f1347786eb4408
prateek1998 <37973089+prateek1998@users.noreply.github.com>
2020-04-03 17:27:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".txt-rotate", {
+  strings: [
+    "I'm a Web Developer",
+    "I'm a Game Developer",
+    "I'm a Machine Learning Enthuasist",
+    "I'm Competitive Programmer"
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  fadeOut: false,
+  smartBackspace: false,
+  loop: true
+});
